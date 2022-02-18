33e4844d88aed5c9d870d47434d8cb3169dc2b52
ihounie <ihounie@gmail.com>
2022-02-18 10:57:29 -0500

    Fixed. OOM was created by passing num_cores>1, which is intended for multiple GPUs/TPUs not CPU cores as I assumed

--- cifar_exp.sh
+++ cifar_exp.sh
@@ -2,5 +2,5 @@ python baselines/cifar/deterministic.py \
     --data_dir=/tmp/tensorflow_datasets \
     --output_dir=/tmp/model \
     --use_gpu=True \
-    --num_cores=32 \
+    --num_cores=1 \
     --download_data=True \
